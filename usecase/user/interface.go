cbd993b3350cf925784f770c3ca96b30d80c0d08
William Ta <wta@student.42.fr>
2021-04-03 06:51:28 +0200

    feat: implement base user usecase

---
+++ usecase/user/interface.go
@@ -0,0 +1,22 @@
+package user
+
+import (
+	"github.com/MonsieurTa/hypertube/entity"
+	"github.com/google/uuid"
+)
+
+type Reader interface{}
+
+type Writer interface {
+	Create(e *entity.User) (uuid.UUID, error)
+}
+
+type Repository interface {
+	Reader
+	Writer
+}
+
+//UseCase interface
+type UseCase interface {
+	CreateUser(c entity.User) (uuid.UUID, error)
+}
---
+++ usecase/user/service.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"github.com/MonsieurTa/hypertube/entity"
+	"github.com/google/uuid"
+)
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *Service {
+	return &Service{
+		repo,
+	}
+}
+
+func (s *Service) CreateUser(model entity.User) (uuid.UUID, error) {
+	id, err := s.repo.Create(&model)
+	if err != nil {
+		return id, err
+	}
+	return id, nil
+}
