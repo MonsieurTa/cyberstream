e8e0ccfec5cd5132502e4a9cc9a7391d2b21b2e0
William Ta <wta@student.42.fr>
2021-04-07 06:00:43 +0200

    config: update 42 redirect uri

--- config/config_dev.go
+++ config/config_dev.go
@@ -11,6 +11,6 @@ const (
 
 	PROVIDER_42_CLIENT_ID    = "f27a14bd15ac22eb8c548f3cb64147b40dfb85e17f071801b3fc2986ac4fe5e5"
 	PROVIDER_42_SECRET       = "f98041bd1755f83a7a8b18892623b2ff8f591ae3f40de12e6c1ad1d24b5e665e"
-	PROVIDER_42_REDIRECT_URI = "http://localhost:3000/api/auth/fortytwo"
+	PROVIDER_42_REDIRECT_URI = "http://localhost:3000/api/auth/fortytwo/callback"
 	PROVIDER_42_AUTH_URI     = "https://api.intra.42.fr/oauth/authorize"
 )
