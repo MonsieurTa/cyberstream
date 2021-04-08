1fc27f1dd40c086aa8c030b3bcad1ee248ddc7ca
William Ta <wta@student.42.fr>
2021-04-08 07:21:51 +0200

    feat: implement credential validator

---
+++ api/validator/credential.go
@@ -0,0 +1,41 @@
+package validator
+
+import (
+	"github.com/MonsieurTa/hypertube/api/common"
+	"github.com/gin-gonic/gin"
+)
+
+type UserCredentialValidator struct {
+	Credential struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	} `json:"credential"`
+
+	output CredentialOutput `json:"-"`
+}
+
+type CredentialOutput struct {
+	Username string
+	Password string
+}
+
+func NewUserCredentialValidator() *UserCredentialValidator {
+	return &UserCredentialValidator{}
+}
+
+func (v *UserCredentialValidator) Validate(c *gin.Context) error {
+	err := common.Bind(c, v)
+	if err != nil {
+		return err
+	}
+	v.output.Username = v.Credential.Username
+	v.output.Password = v.Credential.Password
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (v UserCredentialValidator) Value() CredentialOutput {
+	return v.output
+}
