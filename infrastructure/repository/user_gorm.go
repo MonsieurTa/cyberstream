e4a1db211a7e5af799bcf47527207b9df3291662
William Ta <wta@student.42.fr>
2021-04-11 22:45:16 +0200

    refactor: merge credential repo with user repo

--- infrastructure/repository/user_gorm.go
+++ infrastructure/repository/user_gorm.go
@@ -14,9 +14,28 @@ func NewUserGORM(db *gorm.DB) *UserGORM {
 	return &UserGORM{db}
 }
 
-func (u UserGORM) Create(user *entity.User) (uuid.UUID, error) {
+func (u UserGORM) Create(user *entity.User) (*uuid.UUID, error) {
 	result := u.db.Create(user)
 	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user.ID, nil
+}
+
+func (u UserGORM) CredentialsExist(username, password string) (*uuid.UUID, error) {
+	var credential entity.Credential
+
+	err := u.db.Where("username = ?", username).First(&credential).Error
+	if err != nil {
+		return nil, err
+	}
+
+	err = credential.CheckPassword(password)
+	if err != nil {
+		return nil, err
+	}
+	return &credential.UserID, nil
+}
 		return user.ID, result.Error
 	}
 	return user.ID, nil
