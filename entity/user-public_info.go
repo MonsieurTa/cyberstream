b7cc51dabb8a1c417e5076e3b49b48ff7560fb5e
William Ta <wta@student.42.fr>
2021-04-11 22:46:17 +0200

    feat(entity/user): add picture_url field

--- entity/user-public_info.go
+++ entity/user-public_info.go
@@ -7,13 +7,14 @@ import (
 )
 
 type PublicInfo struct {
-	ID        uuid.UUID `gorm:"column:id;type:uuid;not null"`
-	UserID    uuid.UUID `gorm:"column:user_id"`
-	FirstName string    `gorm:"column:first_name"`
-	LastName  string    `gorm:"column:last_name"`
-	Phone     string    `gorm:"column:phone"`
-	Email     string    `gorm:"column:email;unique"`
-	UpdatedAt time.Time `gorm:"column:updated_at"`
+	ID         uuid.UUID `gorm:"column:id;type:uuid;not null"`
+	UserID     uuid.UUID `gorm:"column:user_id"`
+	FirstName  string    `gorm:"column:first_name"`
+	LastName   string    `gorm:"column:last_name"`
+	Phone      string    `gorm:"column:phone"`
+	Email      string    `gorm:"column:email;unique"`
+	PictureURL string    `gorm:"column:picture_url"`
+	UpdatedAt  time.Time `gorm:"column:updated_at"`
 }
 
 func NewPublicInfo(userID uuid.UUID, firstname, lastname, phone, email string) *PublicInfo {
