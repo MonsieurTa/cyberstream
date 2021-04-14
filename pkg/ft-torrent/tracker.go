cfaf8a032d145ecd7b9567997685aee2398974c1
William Ta <wta@student.42.fr>
2021-04-14 09:22:01 +0200

    refactor: rename struct and enhance error

--- pkg/ft-torrent/tracker.go
+++ pkg/ft-torrent/tracker.go
@@ -21,10 +21,14 @@ type Tracker struct {
 	baseURL    *url.URL
 }
 
-type PeerResponse struct {
+type TrackerResponse struct {
 	Interval int
 	Peers    string
 }
+
+var (
+	err_malformed_response = errors.New("malformed tracker response")
+)
 
 func (t *Tracker) url(infoHash [20]byte) (string, error) {
 	params := url.Values{
@@ -40,51 +44,51 @@ func (t *Tracker) url(infoHash [20]byte) (string, error) {
 	return t.baseURL.String(), nil
 }
 
-func (t *Tracker) RequestPeers(infoHash [20]byte) (PeerResponse, error) {
+func (t *Tracker) RequestPeers(infoHash [20]byte) (TrackerResponse, error) {
 	url, err := t.url(infoHash)
 
 	if err != nil {
-		return PeerResponse{}, err
+		return TrackerResponse{}, err
 	}
 
 	client := http.Client{Timeout: 15 * time.Second}
 	resp, err := client.Get(url)
 	if err != nil {
-		return PeerResponse{}, err
+		return TrackerResponse{}, err
 	}
 
 	defer resp.Body.Close()
 	rawBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return PeerResponse{}, err
+		return TrackerResponse{}, err
 	}
 
 	rawData, err := bencode.Unmarshal(rawBody)
 	if err != nil {
-		return PeerResponse{}, err
+		return TrackerResponse{}, err
 	}
 
 	data, ok := rawData.(map[string]interface{})
 	if !ok {
-		return PeerResponse{}, errors.New("malformed tracker response")
+		return TrackerResponse{}, err_malformed_response
 	}
-	return NewPeerResponse(data)
+	return NewTrackerResponse(data)
 }
 
-func NewPeerResponse(data map[string]interface{}) (PeerResponse, error) {
-	rv := PeerResponse{}
+func NewTrackerResponse(data map[string]interface{}) (TrackerResponse, error) {
+	rv := TrackerResponse{}
 
 	rawInterval, ok1 := data["interval"]
 	interval, ok2 := rawInterval.(int64)
 	if !ok1 || !ok2 {
-		return PeerResponse{}, errors.New("malformed tracker response")
+		return TrackerResponse{}, err_malformed_response
 	}
 	rv.Interval = int(interval)
 
 	rawPeers, ok1 := data["peers"]
 	peers, ok2 := rawPeers.([]byte)
 	if !ok1 || !ok2 {
-		return PeerResponse{}, errors.New("malformed tracker response")
+		return TrackerResponse{}, err_malformed_response
 	}
 	rv.Peers = string(peers[:])
 	return rv, nil
