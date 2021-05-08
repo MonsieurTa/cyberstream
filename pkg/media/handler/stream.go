55f11cfd9208444278cdac0c74b276fbb282400d
William Ta <wta@student.42.fr>
2021-05-08 20:43:28 +0200

    refactor: enhance stream

--- pkg/media/handler/stream.go
+++ pkg/media/handler/stream.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/MonsieurTa/hypertube/common/entity"
@@ -19,12 +20,6 @@ const (
 	ERR_MAGNET         = "error_magnet"
 	ERR_TORRENT_CLIENT = "error_torrent_client"
 	ERR_VALIDATION     = "error_validation"
-)
-
-var (
-	STATIC_FILES_HOST string
-	STATIC_FILES_PORT string
-	STATIC_FILES_PATH string
 )
 
 // TODO create Stream service
@@ -50,23 +45,32 @@ func Stream(c *gin.Context) {
 		})
 		return
 	}
-	defer tc.Close()
 
 	t, err := tc.AddMagnet(magnet)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			ERR_MAGNET: err.Error(),
 		})
+		tc.Close()
 		return
 	}
 
 	<-t.GotInfo()
 
 	h := sha1.Sum([]byte(t.Info().Name))
+	dir := hex.EncodeToString(h[:])
+	filepath := dir + "/" + "out.m3u8"
 	hlsPath := os.Getenv("STATIC_FILES_PATH") + "/" + filepath
 
-	ready := toHLS(t, path)
-	defer close(ready)
+	os.Mkdir(os.Getenv("STATIC_FILES_PATH")+"/"+dir, os.ModePerm)
+
+	ready, done := toHLS(t, hlsPath)
+	go func() {
+		<-done
+		close(ready)
+		close(done)
+		tc.Close()
+	}()
 
 	<-ready
 
@@ -76,8 +80,10 @@ func Stream(c *gin.Context) {
 	c.JSON(http.StatusOK, &resp)
 }
 
-func toHLS(t *torrent.Torrent, path string) chan bool {
+func toHLS(t *torrent.Torrent, path string) (chan bool, chan bool) {
 	rpipe, wpipe, wait := hls.Init(path)
+	ready := make(chan bool)
+	done := make(chan bool)
 
 	progress := make(chan int64)
 	go func() {
@@ -101,16 +107,16 @@ func toHLS(t *torrent.Torrent, path string) chan bool {
 			case progress <- at:
 			default:
 			}
-
 		}
+		done <- true
+		log.Println(path, ": done")
+
 		r.Close()
-		close(progress)
 		rpipe.Close()
 		wpipe.Close()
+		close(progress)
 		wait()
 	}()
-
-	ready := make(chan bool)
 
 	// send true to ready when 1% downloaded
 	go func() {
@@ -122,6 +128,7 @@ func toHLS(t *torrent.Torrent, path string) chan bool {
 			time.Sleep(time.Microsecond * 100)
 		}
 		ready <- true
+		log.Println(path, ": ready")
 	}()
-	return ready
+	return ready, done
 }
