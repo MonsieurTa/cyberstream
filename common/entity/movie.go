3c04ba977e54b602d2ba721f6889d1a1d00156ab
wta <wta@student.42.fr>
2021-05-13 16:44:01 +0200

    refactor: enhance movie entity

--- common/entity/movie.go
+++ common/entity/movie.go
@@ -5,8 +5,8 @@ import "github.com/google/uuid"
 type Movie struct {
 	ID        uuid.UUID `gorm:"column:id;type:uuid;not null" json:"id"`
 	Name      string    `gorm:"column:name;unique;not null" json:"name"`
-	Path      string    `gorm:"column:path;unique;not null" json:"path"`
-	CreatedAt string    `gorm:"column:created_at" json:"created_at"`
+	Path      string    `gorm:"column:path;unique;not null" json:"path,omitempty"`
+	CreatedAt string    `gorm:"column:created_at" json:"created_at,omitempty"`
 
 	Magnet string `gorm:"-" json:"magnet"`
 }
