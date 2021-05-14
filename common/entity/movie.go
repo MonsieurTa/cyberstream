432e038bd5e20446a7d4336bc39130f5e0b62728
wta <wta@student.42.fr>
2021-05-14 17:41:00 +0200

    refactor: change CreatedAt type to time.Time

--- common/entity/movie.go
+++ common/entity/movie.go
@@ -1,21 +1,25 @@
 package entity
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Movie struct {
 	ID        uuid.UUID `gorm:"column:id;type:uuid;not null" json:"id"`
 	Name      string    `gorm:"column:name;unique;not null" json:"name"`
 	Path      string    `gorm:"column:path;unique;not null" json:"path,omitempty"`
-	CreatedAt string    `gorm:"column:created_at" json:"created_at,omitempty"`
+	CreatedAt time.Time `gorm:"column:created_at" json:"created_at,omitempty"`
 
 	Magnet string `gorm:"-" json:"magnet"`
 }
 
 func NewMovie(name, path, magnet string) *Movie {
 	id := uuid.New()
-	return &Movie{id, name, path, "", magnet}
+	return &Movie{id, name, path, time.Time{}, magnet}
 }
 
 func (m *Movie) Stored() bool {
-	return m.CreatedAt != ""
+	return !m.CreatedAt.IsZero()
 }
