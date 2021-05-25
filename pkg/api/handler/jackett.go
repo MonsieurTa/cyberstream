ba690ef8f01192aba0b9fed8bba07bac1028a933
wta <wta@student.42.fr>
2021-05-25 17:54:58 +0200

    feat: encrypt jackett results

--- pkg/api/handler/jackett.go
+++ pkg/api/handler/jackett.go
@@ -2,13 +2,16 @@ package handler
 
 import (
 	"net/http"
+	"os"
 
+	"github.com/MonsieurTa/hypertube/common/cipher"
 	"github.com/MonsieurTa/hypertube/common/validator"
-	"github.com/MonsieurTa/hypertube/pkg/api/usecase/jackett"
+	jackettService "github.com/MonsieurTa/hypertube/pkg/api/usecase/jackett"
 	"github.com/gin-gonic/gin"
+	"github.com/webtor-io/go-jackett"
 )
 
-func JackettSearch(service jackett.UseCase) gin.HandlerFunc {
+func JackettSearch(service jackettService.UseCase) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		validator := validator.NewSearchValidator()
 		err := validator.Validate(c)
@@ -19,23 +22,30 @@ func JackettSearch(service jackett.UseCase) gin.HandlerFunc {
 
 		pattern, categories := validator.Value()
 
-		res, err := service.Search(pattern, categories)
+		resp, err := service.Search(pattern, categories)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
-		c.JSON(http.StatusOK, res)
+
+		rv, err := encryptResultsMagnets(resp.Results)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, rv)
 	}
 }
 
-func JackettCategories(service jackett.UseCase) gin.HandlerFunc {
+func JackettCategories(service jackettService.UseCase) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		cats := service.Categories()
 		c.JSON(http.StatusOK, cats)
 	}
 }
 
-func JackettIndexers(service jackett.UseCase) gin.HandlerFunc {
+func JackettIndexers(service jackettService.UseCase) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		idxs, err := service.ConfiguredIndexers()
 		if err != nil {
@@ -44,4 +54,22 @@ func JackettIndexers(service jackett.UseCase) gin.HandlerFunc {
 		}
 		c.JSON(http.StatusOK, idxs)
 	}
+}
+
+func encryptResultsMagnets(results []jackett.Result) ([]jackett.Result, error) {
+	rv := make([]jackett.Result, 0, len(results))
+
+	c, err := cipher.NewCryptograph(os.Getenv("AES_KEY"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range results {
+		v.MagnetUri, err = c.Encrypt([]byte(v.MagnetUri))
+		if err != nil {
+			return nil, err
+		}
+		rv = append(rv, v)
+	}
+	return rv, nil
 }
