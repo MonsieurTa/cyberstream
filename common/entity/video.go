d8d9754afbe2cfe36644bf5d41fdf20cd0989b59
William Ta <wta@student.42.fr>
2021-06-08 13:20:39 +0200

    refactor: enhance Video data structure

--- common/entity/video.go
+++ common/entity/video.go
@@ -4,21 +4,23 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 type Video struct {
-	ID        uuid.UUID `gorm:"column:id;type:uuid;not null" json:"id"`
-	Name      string    `gorm:"column:name;unique;not null" json:"name"`
-	Hash      string    `gorm:"column:hash;unique;not null" json:"hash"`
-	Path      string    `gorm:"column:path;unique;not null" json:"path,omitempty"`
-	CreatedAt time.Time `gorm:"column:created_at" json:"created_at,omitempty"`
+	ID             uuid.UUID      `gorm:"column:id;type:uuid;not null" json:"id"`
+	Name           string         `gorm:"column:name;unique;not null" json:"name"`
+	Hash           string         `gorm:"column:hash;unique;not null" json:"hash"`
+	FilePath       string         `gorm:"column:file_path;unique;not null" json:"file_path,omitempty"`
+	SubtitlesPaths pq.StringArray `gorm:"type:text[];column:subtitles_paths;unique" json:"subtitle_path,omitempty"`
+	CreatedAt      time.Time      `gorm:"column:created_at" json:"created_at,omitempty"`
 
 	Magnet string `gorm:"-" json:"magnet"`
 }
 
-func NewVideo(name, hash, path, magnet string) *Video {
+func NewVideo(name, hash, filePath, magnet string, subtitlesPaths []string) *Video {
 	id := uuid.New()
-	return &Video{id, name, hash, path, time.Time{}, magnet}
+	return &Video{id, name, hash, filePath, subtitlesPaths, time.Time{}, magnet}
 }
 
 func (m *Video) Stored() bool {
