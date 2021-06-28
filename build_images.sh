a075c094c08ae6b0dda26246de1256cff2b37d87
William Ta <wta@student.42.fr>
2021-06-28 12:20:47 +0200

    chore: update build images script

--- build_images.sh
+++ build_images.sh
@@ -1,2 +1,7 @@
-docker build -t hypertube-api -f cmd/api/Dockerfile .
-docker build -t hypertube-media -f cmd/media/Dockerfile .
+docker build -t mrta/hypertube-api:latest -f cmd/api/Dockerfile .
+docker build -t mrta/hypertube-media:latest -f cmd/media/Dockerfile .
+docker build -t mrta/hypertube-transcoder:latest -f cmd/transcoder/Dockerfile .
+
+docker push mrta/hypertube-api:latest
+docker push mrta/hypertube-media:latest
+docker push mrta/hypertube-transcoder:latest
