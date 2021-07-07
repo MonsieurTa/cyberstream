8652044f92cfe7a5c90d0ec2e1ed8fe8f6a6f729
William Ta <wta@student.42.fr>
2021-07-07 13:57:00 +0200

    chore: add docker buildkit variable

--- build_images.sh
+++ build_images.sh
@@ -1,3 +1,3 @@
-docker build -t mrta/hypertube-api:latest -f cmd/api/Dockerfile .
-docker build -t mrta/hypertube-media:latest -f cmd/media/Dockerfile .
-docker build -t mrta/hypertube-transcoder:latest -f cmd/transcoder/Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t mrta/hypertube-api:latest -f cmd/api/Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t mrta/hypertube-media:latest -f cmd/media/Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t mrta/hypertube-transcoder:latest -f cmd/transcoder/Dockerfile .
